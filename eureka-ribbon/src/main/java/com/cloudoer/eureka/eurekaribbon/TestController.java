42165d37584c9181d89e91d2a49ce7be47685295
liuxiaokun <liuxk@cloudoer.com>
2018-09-10 16:48:57 +0800

    add Hystrix

--- eureka-ribbon/src/main/java/com/cloudoer/eureka/eurekaribbon/TestController.java
+++ eureka-ribbon/src/main/java/com/cloudoer/eureka/eurekaribbon/TestController.java
@@ -1,5 +1,6 @@
 package com.cloudoer.eureka.eurekaribbon;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,9 +19,14 @@ public class TestController {
     private RestTemplate restTemplate;
 
     @GetMapping("/loadHi")
+    @HystrixCommand(fallbackMethod = "hiError")
     public Object loadHi(@RequestParam String name) {
 
         return restTemplate.getForObject("http://service-fred/hi?name=" + name,
                 String.class);
+    }
+
+    public String hiError(String name) {
+        return "hi," + name + ",sorry,error!";
     }
 }
