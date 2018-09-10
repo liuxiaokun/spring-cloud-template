2530cff7dd7459945d70c88f30e3e08b3126dcfd
liuxiaokun <liuxk@cloudoer.com>
2018-09-10 15:56:43 +0800

    add load balance, using ribbon

---
+++ eureka-ribbon/src/main/java/com/cloudoer/eureka/eurekaribbon/EurekaRibbonApplication.java
@@ -0,0 +1,28 @@
+package com.cloudoer.eureka.eurekaribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+/**
+ * @author liuxiaokun
+ * @version 0.0.1
+ * @since 2018/9/10
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaRibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaRibbonApplication.class, args);
+    }
+
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
---
+++ eureka-ribbon/src/main/java/com/cloudoer/eureka/eurekaribbon/TestController.java
@@ -0,0 +1,26 @@
+package com.cloudoer.eureka.eurekaribbon;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author liuxiaokun
+ * @version 0.0.1
+ * @since 2018/9/10
+ */
+@RestController
+public class TestController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/loadHi")
+    public Object loadHi(@RequestParam String name) {
+
+        return restTemplate.getForObject("http://service-fred/hi?name=" + name,
+                String.class);
+    }
+}
