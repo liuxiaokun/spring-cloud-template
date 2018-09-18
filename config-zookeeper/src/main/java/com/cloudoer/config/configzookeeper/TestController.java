9b30bfe2d54c23220927ce12441ff83120023a87
liuxiaokun <liuxk@cloudoer.com>
2018-09-18 09:18:40 +0800

    config eureka server HA

--- config-zookeeper/src/main/java/com/cloudoer/config/configzookeeper/TestController.java
+++ config-zookeeper/src/main/java/com/cloudoer/config/configzookeeper/TestController.java
@@ -2,6 +2,7 @@ package com.cloudoer.config.configzookeeper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2018/9/11
  */
 @RestController
+@RefreshScope
 public class TestController {
 
     @Value("${server.port}")
