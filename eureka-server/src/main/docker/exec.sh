93ca2566c2af869e0c441fd646de8f4aa16ef07a
liuxiaokun <liuxk@cloudoer.com>
2018-10-12 14:51:38 +0800

    add deploy script

---
+++ eureka-server/src/main/docker/exec.sh
@@ -0,0 +1,7 @@
+#用maven对项目进行打包
+mvn clean package
+# docker.cloudoer.com/eureka-server是build的镜像名字，后面的点别忘了，路径。
+docker build -t docker.cloudoer.com/eureka-server .
+#启动镜像，根据不同profile
+docker run --name eureka-server-1 -e "SPRING_PROFILES_ACTIVE=peer1" -p 8761:8761 -itd docker.cloudoer.com/eureka-server
+docker run --name eureka-server-2 -e "SPRING_PROFILES_ACTIVE=peer2" -p 8762:8762 -itd docker.cloudoer.com/eureka-server
