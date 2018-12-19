c18ec25fc6d2109ea0edf707f15770a375faac74
liuxiaokun <liuxk@cloudoer.com>
2018-12-19 10:54:41 +0800

    commit

--- eureka-zuul/src/main/java/com/cloudoer/eureka/eurekazuul/EurekaZuulApplication.java
+++ eureka-zuul/src/main/java/com/cloudoer/eureka/eurekazuul/EurekaZuulApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+/**
+ * @author liuxiaokun
+ * @version 0.0.1
+ * @since 2018/9/10
+ */
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
